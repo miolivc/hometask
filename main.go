3c1432bd6e0dfd331f1cc5427c86c73f80a6c23b
Michelle Oliveira <michelleolivcosta@gmail.com>
2021-09-05 08:29:53 -0300

    * Adding POST method to Task

--- main.go
+++ main.go
@@ -7,25 +7,39 @@ import (
 )
 
 type task struct {
-	ID    string `json:"id"`
+	ID    int64  `json:"id"`
 	Name  string `json:"name"`
 	Level int16  `json:"level"`
 	Daily bool   `json:"daily"`
 }
 
 var tasks = []task{
-	{ID: "1", Name: "Limpar chão da cozinha", Level: 2, Daily: false},
-	{ID: "1", Name: "Limpar chão da varanda", Level: 2, Daily: false},
-	{ID: "1", Name: "Limpar caixa de areia", Level: 1, Daily: true},
+	{ID: 1, Name: "Limpar chão da cozinha", Level: 2, Daily: false},
+	{ID: 2, Name: "Limpar chão da varanda", Level: 2, Daily: false},
+	{ID: 3, Name: "Limpar caixa de areia", Level: 1, Daily: true},
 }
 
 func getTasks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, tasks)
 }
 
+func postTasks(c *gin.Context) {
+	var newTask task
+
+	// Call BindJSON to bind the received JSON
+	if err := c.BindJSON(&newTask); err != nil {
+		return
+	}
+
+	// Add the new task to the slice.
+	tasks = append(tasks, newTask)
+	c.IndentedJSON(http.StatusCreated, newTask)
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/tasks", getTasks)
+	router.POST("/tasks", postTasks)
 
 	router.Run("localhost:8080")
 }
